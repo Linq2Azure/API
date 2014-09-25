05f523667c24107c2478d93b0b8f6903ff16569f
Colin Scott <colin.scott@cashconverters.com>
2014-09-25 18:48:21 +0800

    Breaking change that stores configuration in XML

--- CloudServices/ServiceConfiguration.cs
+++ CloudServices/ServiceConfiguration.cs
@@ -1,96 +1,175 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace Linq2Azure.CloudServices
 {
     public class ServiceConfiguration
     {
+        private readonly XElement _existingConfiguration;
+        private readonly IEnumerable<RoleConfiguration> _roles;
+
         public string ServiceName { get; set; }
         public int OsFamily { get; set; }
         public string OsVersion { get; set; }
-        public List<RoleConfiguration> ConfigurationItems { get; set; }
 
-        public ServiceConfiguration()
+        public IEnumerable<RoleConfiguration> Roles
         {
-            ConfigurationItems = new List<RoleConfiguration>();
-            OsVersion = "*";
+            get { return _roles; }
         }
 
-        public ServiceConfiguration(string configData) : this(XElement.Parse(configData)) { }
+        public ServiceConfiguration(string configDataPath)
+            : this(XElement.Parse(configDataPath))
+        {}
 
         public ServiceConfiguration(XElement configData)
         {
-            ServiceName = (string)configData.Attribute("serviceName");
-            OsFamily = (int?)configData.Attribute("osFamily") ?? 0;
-            OsVersion = (string)configData.Attribute("osVersion");
+            _existingConfiguration = new XElement(configData);
+            ServiceName = (string)_existingConfiguration.Attribute("serviceName");
+            OsFamily = (int?)_existingConfiguration.Attribute("osFamily") ?? 0;
+            OsVersion = (string)_existingConfiguration.Attribute("osVersion");
 
-            ConfigurationItems = new List<RoleConfiguration>(
-                configData.Elements(XmlNamespaces.ServiceConfig + "Role").Select(RoleConfiguration.Load));
+            _roles = new List<RoleConfiguration>(
+                _existingConfiguration.Elements(XmlNamespaces.ServiceConfig + "Role")
+                    .Select(element => new RoleConfiguration(element)))
+               .AsReadOnly();
         }
 
         public XElement ToXml()
         {
-            return new XElement(XmlNamespaces.ServiceConfig + "ServiceConfiguration",
-                new XAttribute("serviceName", ServiceName),
-                new XAttribute("osFamily", OsFamily),
-                new XAttribute("osVersion", OsVersion),
-                new XAttribute("schemaVersion", "2012-05.1.7"),
-                ConfigurationItems.Select(c => c.ToXml())
-                );
+            _existingConfiguration.SetAttributeValue("serviceName", ServiceName);
+            _existingConfiguration.SetAttributeValue("osFamily", OsFamily.ToString(CultureInfo.InvariantCulture));
+            _existingConfiguration.SetAttributeValue("osVersion", OsVersion);
+
+            return _existingConfiguration;
         }
     }
 
     public class RoleConfiguration
     {
-        public XElement ToXml()
+        private readonly XElement _roleConfiguration;
+
+        internal RoleConfiguration(XElement roleConfiguration)
         {
-            return new XElement(XmlNamespaces.ServiceConfig + "Role", new XAttribute("name", RoleName),
-                new XElement(XmlNamespaces.ServiceConfig + "Instances", new XAttribute("count", InstanceCount)),
-                new XElement(XmlNamespaces.ServiceConfig + "ConfigurationSettings",
-                    ConfigurationSettings.Select(kvp => new XElement(XmlNamespaces.ServiceConfig + "Setting", new XAttribute("name", kvp.Key), new XAttribute("value", kvp.Value)))),
-                Certificates == null || Certificates.Count == 0 ? null : new XElement(XmlNamespaces.ServiceConfig + "Certificates", Certificates.Values.Select(c => c.ToXml()))
-                );
+            _roleConfiguration = roleConfiguration;
+            ConfigurationSettings = new RoleConfigurationSettings(_roleConfiguration.Element(XmlNamespaces.ServiceConfig + "ConfigurationSettings"));
+            Certificates = new RoleCertificateConfigurations(_roleConfiguration.Element(XmlNamespaces.ServiceConfig + "Certificates"));
         }
 
-        public static RoleConfiguration Load(XElement element)
+        public string RoleName
         {
-            var rc = new RoleConfiguration();
-            rc.RoleName = (string)element.Attribute("name");
-            rc.InstanceCount = (int)element.Elements(XmlNamespaces.ServiceConfig + "Instances").Single().Attribute("count");
-            rc.ConfigurationSettings = element.Elements(XmlNamespaces.ServiceConfig + "ConfigurationSettings").SelectMany(d => d.Elements()).ToDictionary(x => (string)x.Attribute("name"), x => (string)x.Attribute("value"));
-            rc.Certificates = element.Elements(XmlNamespaces.ServiceConfig + "Certificates").SelectMany(d => d.Elements()).ToDictionary(x => (string)x.Attribute("name"), x => new CertificateConfig(x));
-            return rc;
+            get { return (string)_roleConfiguration.Attribute("name"); }
+            set { _roleConfiguration.SetAttributeValue("name", value); }
         }
 
-        public string RoleName { get; set; }
-        public int InstanceCount { get; set; }
-        public IDictionary<string, string> ConfigurationSettings { get; set; }
-        public IDictionary<string, CertificateConfig> Certificates { get; set; }
+        public int InstanceCount
+        {
+            get { return (int)_roleConfiguration.Elements(XmlNamespaces.ServiceConfig + "Instances").Single().Attribute("count"); }
+            set { _roleConfiguration.Elements(XmlNamespaces.ServiceConfig + "Instances").Single().SetAttributeValue("count", value); }
+        }
+
+        public RoleConfigurationSettings ConfigurationSettings { get; private set; }
+        public RoleCertificateConfigurations Certificates { get; private set; }
     }
 
-    public class CertificateConfig
+    public class RoleConfigurationSettings
     {
-        public string Name { get; set; }
-        public string Thumbprint { get; set; }
-        public string ThumbprintAlgorithm { get; set; }
+        private readonly XElement _settingsElement;
 
-        public XElement ToXml()
+        public RoleConfigurationSettings(XElement settingsElement)
         {
-            return new XElement(XmlNamespaces.ServiceConfig + "Certificate",
-                new XAttribute("name", Name),
-                new XAttribute("thumbprint", Thumbprint),
-                new XAttribute("thumbprintAlgorithm", ThumbprintAlgorithm));
+            _settingsElement = settingsElement;
         }
 
-        public CertificateConfig(XElement xml)
+        public string this[string key]
         {
-            Name = (string)xml.Attribute("name");
-            Thumbprint = (string)xml.Attribute("thumbprint");
-            ThumbprintAlgorithm = (string)xml.Attribute("thumbprintAlgorithm");
+            get
+            {
+                var element = GetSettingElement(key);
+
+                return element.Attribute("value").Value;
+            }
+            set
+            {
+                var element = GetSettingElement(key);
+
+                element.SetAttributeValue("value", value);
+            }
+        }
+
+        private XElement GetSettingElement(string key)
+        {
+            var element = _settingsElement.Elements()
+                .FirstOrDefault(e => e.Attribute("name").Value == key);
+
+            if (element == null)
+            {
+                throw new InvalidOperationException(
+                    string.Format(
+                        CultureInfo.InvariantCulture,
+                        "Role does not have setting with key '{0}'",
+                        key));
+            }
+            return element;
+        }
+    }
+
+    public class RoleCertificateConfigurations
+    {
+        private readonly XElement _certificatesConfigurationElement;
+
+        public RoleCertificateConfigurations(XElement certificatesConfigurationElement)
+        {
+            _certificatesConfigurationElement = certificatesConfigurationElement;
+        }
+
+        public CertificateConfiguration this[string key]
+        {
+            get
+            {
+                var element = _certificatesConfigurationElement.Elements()
+                    .FirstOrDefault(e => e.Attribute("name").Value == key);
+
+                if (element == null)
+                {
+                    throw new InvalidOperationException(
+                        string.Format(
+                            CultureInfo.InvariantCulture,
+                            "Role does not have a certificate with key '{0}'",
+                            key));
+                }
+
+                return new CertificateConfiguration(element);
+            }
+        }
+    }
+
+    public class CertificateConfiguration
+    {
+        private readonly XElement _certificateConfigurationElement;
+
+        internal CertificateConfiguration(XElement certificateConfigurationElement)
+        {
+            _certificateConfigurationElement = certificateConfigurationElement;
+        }
+
+        public string Name
+        {
+            get { return (string)_certificateConfigurationElement.Attribute("name"); }
+        }
+
+        public string Thumbprint
+        {
+            get { return (string)_certificateConfigurationElement.Attribute("thumbprint"); }
+            set { _certificateConfigurationElement.SetAttributeValue("thumbprint", value); }
+        }
+
+        public string ThumbprintAlgorithm
+        {
+            get { return (string)_certificateConfigurationElement.Attribute("thumbprintAlgorithm"); }
+            set { _certificateConfigurationElement.SetAttributeValue("thumbprintAlgorithm", value); }
         }
     }
 }
